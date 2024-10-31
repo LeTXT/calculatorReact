0961dd2d554c8d95f4c17c9783de0c8dfb994419
Leandro <leandrogomesdeoliveira2@gmail.com>
2024-10-30 21:35:21 -0300

    start calculator in react

---
+++ src/components/Screen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/components/Screen.module.css";
+
+function Screen() {
+    return (
+        <div className="screenContainer">
+            <input type="text" name="" id="" />
+            <input type="text" />
+        </div>
+    )
+}
+
+export default Screen;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "../styles/pages/Home.module.css"
+
+import Screen from "../components/Screen";
+
+function Home() {
+    return (
+        <div className="homeContainer">
+            <Screen />
+        </div>
+    )
+}
+
+export default Home;
